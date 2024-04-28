272117cda080329ae442f8be052b15a3e5e443e7
Monjur Hossen <14monjurhossen@gmail.com>
2024-04-29 03:26:31 +0600

    login system added

--- src/Sections/CraftAndArt.jsx
+++ src/Sections/CraftAndArt.jsx
@@ -16,12 +16,24 @@ const CraftAndArt = () => {
 
   return (
     <div className="contain">
-      <h1>This is Art and Craft Section</h1>
-      {
-        allItems.map((item) => (
-          <h1 key={item._id}>{item.item_name}</h1>
-        ))
-      }
+     <div className="my-10 grid grid-cols-3 gap-5">
+     {/* <div className="w-full border p-2 rounded-md">
+      <img src={`https://i.ibb.co/b6LBTGS/ai-generated-8659118-640.jpg`} alt=""  className="h-56 w-full rounded-md"/>
+      <h1 className="text-2xl font-bold my-2">Farm Agriculture</h1>
+      <h3 className="text-lg font-semibold">Price: 99$</h3>
+      <button type="submit"  className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">See Details</button>
+     </div> */}
+     {
+      allItems.map((item) => (
+        <div className="w-full border p-2 rounded-md" key={item._id}>
+      <img src={item.image} alt=""  className="h-56 w-full rounded-md"/>
+      <h1 className="text-2xl font-bold my-2">{item.item_name}</h1>
+      <h3 className="text-lg font-semibold">Price: {item.price}$</h3>
+      <button  className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">See Details</button>
+     </div>
+      ))
+     }
+     </div>      
     </div>
   )
 }
--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -1,15 +1,82 @@
-import Reviews from "../Components/Reviews"
-import Slider from "../Components/Slider"
-import ContactUs from "./ContactUs"
+import { useEffect, useState } from "react";
+import Reviews from "../Components/Reviews";
+import Slider from "../Components/Slider";
+import ContactUs from "./ContactUs";
 
 const Home = () => {
-  return ( 
-    <div className="contain">
-        <Slider/>
-        <ContactUs/>
-        <Reviews/>    
-    </div>
-  )
-}
+  const [allCraft, setAllCraft] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/items")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllCraft(data);
+      });
+  }, []);
+  const newArr = allCraft.slice(0, 6);
 
-export default Home
+  return (
+    <div className="contain">
+      <Slider />
+      <div className="mt-10">
+        <h1 className="text-4xl font-bold text-center">Craft & Art</h1>
+        <div className="my-10 grid grid-cols-3 gap-5">
+          {newArr.map((item) => (
+            <div className="w-full border p-2 rounded-md" key={item._id}>
+              <img src={item.image} alt="" className="h-56 w-full rounded-md object-cover" />
+              <h1 className="text-2xl font-bold my-2">{item.item_name}</h1>
+              <h3 className="text-lg font-semibold">Price: {item.price}$</h3>
+              <button className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">
+                See Details
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* catagory section */}
+      <div>
+        <h1 className="text-4xl font-bold text-center">Catagories</h1>
+        <div className="my-10 grid grid-cols-3 gap-5">
+            <div className="w-full border p-2 rounded-md">
+              <img src='https://i.ibb.co/QNnvNXG/painting-7059647-1280.jpg' alt="" className="h-56 w-full rounded-md object-cover" />
+              <h1 className="text-2xl font-bold my-2">Landscape Painting</h1>
+              <button className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">
+                View
+              </button>
+            </div>
+            <div className="w-full border p-2 rounded-md">
+              <img src='https://i.ibb.co/xMZ4Q1h/woman-8695648-1280.jpg' alt="" className="h-56 w-full rounded-md object-cover" />
+              <h1 className="text-2xl font-bold my-2">Portrait Drawing</h1>
+              <button className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">
+                View
+              </button>
+            </div>
+            <div className="w-full border p-2 rounded-md">
+              <img src='https://i.ibb.co/Tqsdv2c/ai-generated-8702139-1280.jpg' alt="" className="h-56 w-full rounded-md object-cover" />
+              <h1 className="text-2xl font-bold my-2">Watercolour Painting</h1>
+              <button className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">
+                View
+              </button>
+            </div>
+            <div className="w-full border p-2 rounded-md">
+              <img src='https://i.ibb.co/WVNMkSN/ai-generated-8113445-640.jpg' alt="" className="h-56 w-full rounded-md object-cover" />
+              <h1 className="text-2xl font-bold my-2">Oil Painting</h1>
+              <button className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">
+                View
+              </button>
+            </div>
+            <div className="w-full border p-2 rounded-md">
+              <img src='https://i.ibb.co/WVNMkSN/ai-generated-8113445-640.jpg' alt="" className="h-56 w-full rounded-md object-cover" />
+              <h1 className="text-2xl font-bold my-2">Charcoal Sketching</h1>
+              <button className="bg-blue-700 text-white font-semibold text-lg px-6 py-1 rounded-md mt-3">
+                View
+              </button>
+            </div>
+        </div>
+      </div>
+      <ContactUs />
+      <Reviews />
+    </div>
+  );
+};
+
+export default Home;
